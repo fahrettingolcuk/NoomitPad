b9af9059d6cb91c057ec96778e8b76d5287013a4
Fahreddin Gölcük <fahreddingolcuk@gmail.com>
2019-12-22 16:07:42 +0300

    last

---
+++ src/BookList/BookList.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react';
+import { View, Text, FlatList, Image } from 'react-native';
+import SQLite from 'react-native-sqlite-storage';
+import { TouchableOpacity } from 'react-native-gesture-handler';
+
+import { connect } from 'react-redux'
+
+
+const db = SQLite.openDatabase({ name: 'Records.db', location: 'default' });
+class BookList extends React.Component {
+    constructor(props) {
+        super(props);
+        //db.transaction((mydb) => {
+        //    mydb.executeSql('SELECT * FROM Bookss', [], (tx, results) => {
+        //        for (var i = 0; i < results.rows.length; i++) {
+        //             this.setState({ BookItems: [...this.props.BookItems, results.rows.item(i)] })
+        //            //console.log(results.rows.item(i));
+        //        }
+        //    })
+        // })
+    }
+    componentDidUpdate() {
+        console.log('guncellendi');
+    }
+    render() {
+        return (
+            <View>
+                <Text>BOOK List</Text>
+                <FlatList
+                    data={this.props.BookItems}
+                    renderItem={({ item }) => <View>
+                        <Text>{item.book_name}</Text>
+                        <Image
+                            source={{ uri: item.book_uri }}
+                            style={{ width: 250, height: 250 }}
+                        />
+                    </View>}
+                    keyExtractor={item => item.id}
+                />
+            </View>
+        )
+    }
+}
+function mapStateToProps(state) {
+    return {
+        counter: state.counter,
+        BookItems: state.BookItems
+    }
+}
+export default connect(mapStateToProps)(BookList)
---
+++ src/TabNavigator/TabNavigator.js
@@ -0,0 +1,17 @@
+
+import React,{Component} from 'react';
+import {createBottomTabNavigator} from 'react-navigation-tabs'
+import {createAppContainer} from 'react-navigation'
+import CustomTab from '../CustomTab/CustomTab'
+
+
+import HomeScreen from '../HomeScreen/HomeScreen'
+import NewRecord from '../NewRecord/NewRecord'
+import BookList from '../BookList/BookList'
+
+const TabNavigator = createBottomTabNavigator({
+    Home:HomeScreen,
+    NewRec : NewRecord,
+    ListBook : BookList
+  },{tabBarComponent:props=><CustomTab {...props}/>})
+  export default createAppContainer(TabNavigator);
