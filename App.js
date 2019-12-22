9edda0ac0d6569f4256b3c2044d2e380dfb98f6a
Fahreddin Gölcük <fahreddingolcuk@gmail.com>
2019-12-22 16:07:14 +0300

    last

--- App.js
+++ App.js
@@ -1,15 +1,72 @@
 
-import React,{Component} from 'react';
-import {createBottomTabNavigator} from 'react-navigation-tabs'
-import {createAppContainer} from 'react-navigation'
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+import TabNavigator from './src/TabNavigator/TabNavigator';
+import { createStore } from 'redux';
+import ImagePicker from 'react-native-image-picker';
+import { Provider } from 'react-redux'
+import SQLite from 'react-native-sqlite-storage';
+const db = SQLite.openDatabase({ name: 'Records.db', location: 'default' });
+const initialState = {
+  BookItems: [],
+  bookName: 'a',
+  bookAuthor: 'a',
+  bookUri: '',
+  photo: null,
+}
 
-import CustomTab from './src/CustomTab/CustomTab'
-import HomeScreen from './src/HomeScreen/HomeScreen'
-import NewRecord from './src/NewRecord/NewRecord'
-import BookList from './src/BookList/BookList'
-const TabNavigator = createBottomTabNavigator({
-  Home:HomeScreen,
-  NewRec : NewRecord,
-  ListBook : BookList
-},{tabBarComponent:props=><CustomTab {...props}/>})
-export default createAppContainer(TabNavigator);
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case 'SaveBook':
+      db.transaction((mydb) => {
+        mydb.executeSql('INSERT INTO Bookss (book_name,book_author,book_uri) VALUES (:book_name,:book_author,:book_uri)', [state.bookName, state.bookAuthor, state.bookUri])
+        mydb.executeSql('SELECT * FROM Bookss', [], (tx, results) => {
+          for (var i = 0; i < results.rows.length; i++) {
+            return { BookItems: [...state.BookItems, results.rows.item(i)] }
+          }
+        })
+      })
+    case 'UpdateBookName':
+      return [...state,{bookName : action.valueName}]
+    case 'UpdateBookAuthor':
+      return[...state,{bookAuthor: action.valueAuthor}]
+    case 'HandlePhoto':
+      const options = {
+        noData: true
+      };
+      ImagePicker.launchImageLibrary(options, response => {
+        if (response.uri) {
+          console.log(response)
+          return { bookUri: response.uri, photo: response }
+        }
+      })
+  }
+  return state
+}
+
+const store = createStore(reducer)
+
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    db.transaction((mydb) => {
+      mydb.executeSql('SELECT * FROM Bookss', [], (tx, results) => {
+        for (var i = 0; i < results.rows.length; i++) {
+          return { BookItems: [...state.BookItems, results.rows.item(i)] }
+        }
+      })
+    })
+    db.transaction((mydb) => {
+      //mydb.executeSql('DELETE FROM Bookss', []);
+      mydb.executeSql('CREATE TABLE IF NOT EXISTS Bookss(book_id INTEGER PRIMARY KEY NOT NULL,book_name VARCHAR(30),book_author VARCHAR(30),book_uri VARCHAR(30))', []);
+    });
+  }
+  render() {
+    return (
+      <Provider store={store}>
+        {/* <NewRecord/> */}
+        <TabNavigator />
+      </Provider>
+    )
+  }
+}
--- src/NewRecord/NewRecord.js
+++ src/NewRecord/NewRecord.js
@@ -1,47 +1,26 @@
 import React, { Component } from 'react';
-import { View, Text, StyleSheet, Button, Image, TextInput, FlatList, Alert } from 'react-native';
+import { View, Text, StyleSheet, Button, Image, TextInput, FlatList, Alert,TouchableOpacity } from 'react-native';
 import ImagePicker from 'react-native-image-picker';
 import SQLite from 'react-native-sqlite-storage';
-import BookList from '../BookList/BookList'
+
+import {connect} from 'react-redux'
+
 const db = SQLite.openDatabase({ name: 'Records.db', location: 'default' });
-export default class NewRecord extends React.Component {
+class NewRecord extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      photo: null,
-      bookName: '',
-      bookAuthor: '',
-      bookUri:'',
-      Values: [],
-    }
-    db.transaction((mydb) => {
-      //mydb.executeSql('DELETE FROM Bookss', []);
-      mydb.executeSql('CREATE TABLE IF NOT EXISTS Bookss(book_id INTEGER PRIMARY KEY NOT NULL,book_name VARCHAR(30),book_author VARCHAR(30),book_uri VARCHAR(30))', []);
-    });
   }
-  handleChoosePhoto = () => {
-    const options = {
-      noData: true
-    };
-    ImagePicker.launchImageLibrary(options, response => {
-      if (response.uri) {
-        this.setState({bookUri: response.uri})
-        this.setState({ photo: response});
-      }
-    })
-  }
-  saveBook = () => {
-    this.state.Values.length = 0;
-    db.transaction((mydb) => {
-      console.log(this.state.bookAuthor);
-      console.log(this.state.bookUri);
-      mydb.executeSql('INSERT INTO Bookss (book_name,book_author,book_uri) VALUES (:book_name,:book_author,:book_uri)', [this.state.bookName, this.state.bookAuthor,this.state.bookUri]);
-    })
-    Alert.alert('BOOK SAVEDs');
-  }
+  // saveBook = () => {
+  //   db.transaction((mydb) => {
+  //     console.log(this.state.bookAuthor);
+  //     console.log(this.state.bookUri);
+  //     mydb.executeSql('INSERT INTO Bookss (book_name,book_author,book_uri) VALUES (:book_name,:book_author,:book_uri)', [this.state.bookName, this.state.bookAuthor,this.state.bookUri]);
+  //   })
+  //   Alert.alert('BOOK SAVEDs');
+  // }
 
   render() {
-    const { photo } = this.state;
+    const { photo } = this.props; //this props olacak.
     return (
       <View style={styles.container}>
         {photo && (
@@ -52,20 +31,43 @@ export default class NewRecord extends React.Component {
         )}
         <Text>BOOKS NAME</Text>
         <TextInput
-          onChangeText={(value) => this.setState({ bookName: value })}
+          onChangeText={(value) => this.props.bookNameFunct(value)}
           style={{ borderWidth: 1, width: 150 }}
         />
         <Text>BOOK AUTHOR</Text>
         <TextInput
-          onChangeText={(value) => this.setState({ bookAuthor: value })}
+          onChangeText={(value) => this.props.bookAuthorFunct(value)}
+          //propstaki bookauthora at 
           style={{ borderWidth: 1, width: 150 }}
         />
-        <Button title="Choose Photo" onPress={() => this.handleChoosePhoto()} />
-        <Button title="SAVE BOOK" onPress={() => this.saveBook()} />
+        <Button title="Choose Photo" onPress={() => this.props.handleChoosePhoto()} />
+        <Button title="SAVE BOOK" onPress={() => this.props.saveBook()} />
+        <View>
+        <Text>{this.props.bookName}</Text>
+        <Text>--------</Text>
+        <Text>{this.props.bookAuthor}</Text>
+        </View>
       </View>
     );
   }
 }
+function mapStateToProps(state){
+  return{
+    bookName : state.bookName,
+    bookAuthor : state.bookAuthor,
+    bookUri : state.bookUri,
+    photo : state.photo,
+  }
+}
+function mapDispatchToProps(dispatch){
+  return {
+    saveBook : ()=>dispatch({type:'SaveBook'}),
+    bookNameFunct : (myvalueName)=>dispatch({type:'UpdateBookName',valueName : myvalueName}),
+    bookAuthorFunct : (myvalueAuthor)=>dispatch({type:'UpdateBookAuthor',valueAuthor : myvalueAuthor}),
+    handleChoosePhoto : ()=>dispatch({type:'HandlePhoto'}),
+  }
+}
+export default connect(mapStateToProps,mapDispatchToProps)(NewRecord)
 
 const styles = StyleSheet.create({
   container: {
