326121af120c7050a18dabcfec3708e44e658241
Fahreddin Gölcük <fahreddingolcuk@gmail.com>
2019-12-22 17:46:58 +0300

    22.12.2019

--- src/BookList/BookList.js
+++ src/BookList/BookList.js
@@ -11,32 +11,40 @@ class BookList extends React.Component {
         super(props);
         this.state = {
             bookList: [],
+            compleatedSql : false,
         }
         db.transaction((mydb) => {
             mydb.executeSql('SELECT * FROM Books', [], (tx, results) => {
                 for (var i = 0; i < results.rows.length; i++) {
                     this.setState({ bookList: [...this.state.bookList, results.rows.item(i)] })
+                    console.log(this.state.bookList);
                 }
+                this.setState({compleatedSql: true})
             })
         })
+        
     }
     render() {
         return (
             <View>
                 <Text>BOOK List</Text>
-                <FlatList
-                    data={this.state.bookList}
-                    keyExtractor={({ id }, index) => index}
-                    renderItem={({ item, index }) =>
-                        <View>
-                            <Image
-                                source={{ uri: item.book_uri }}
-                                style={{ width: 150, height: 150 }}
-                            />
-                            <Text>{item.book_name}</Text>
-                            <Text>{item.book_author}</Text>
-                        </View>}
-                />
+                {this.state.compleatedSql ? 
+                            <FlatList
+                            data={this.state.bookList}
+                            keyExtractor={({ id }, index) => index}
+                            renderItem={({ item, index }) =>
+                                <View>
+                                    <Image
+                                        source={{ uri: item.book_uri }}
+                                        style={{ width: 150, height: 150 }}
+                                    />
+                            <Text>{item.book_uri}</Text>
+                                    <Text>{item.book_name}</Text>
+                                    <Text>{item.book_author}</Text>
+                                </View>}
+                        />
+                        : <Text>bekle</Text>    
+            }
             </View>
         )
     }
